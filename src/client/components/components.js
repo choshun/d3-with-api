8c3b5d15d3857b9d77e63e6f0efbb76669f9b0a7
Choshun Snyder <choshun.snyder@gmail.com>
2016-08-12 20:22:42 -0400

    renaming component stuff

--- src/client/components/components.js
+++ src/client/components/components.js
@@ -1,8 +1,8 @@
 import angular from 'angular';
 import Controls from './controls/controls';
-import Target from './graph/graph';
+import Graph from './graph/graph';
 
 export default angular.module('app.components', [
 	Controls.name,
-	Target.name
+	Graph.name
 ]);
