e67f5fb89193c695dc7e4eec25888758b4b41b5b
Choshun Snyder <choshun.snyder@gmail.com>
2016-08-14 23:50:21 -0400

    lots of graphing, delta is there

--- src/client/components/graph/graph.service.js
+++ src/client/components/graph/graph.service.js
@@ -9,32 +9,43 @@ class TargetService {
     this.pathClass = 'path';
     this.svg = undefined;
     this.xScale = undefined;
-    this.yScale = undefined;
+    this.twitterYScale = undefined;
+    this.facebookYScale = undefined;
+    this.deltaYScale = undefined;
     this.xAxisGen = undefined;
-    this.yAxisGen = undefined;
+    this.twitterYAxisGen = undefined;
+    this.facebookYAxisGen = undefined;
     this.lineFun = undefined;
 
     this.twitterData = [];
     this.facebookData = [];
+    this.deltaData = [];
   }
 
   graph(rawSvg, data) {
     this.svg = D3.select(rawSvg);
-    this.drawLineChart(rawSvg, data);
+    this.drawCharts(rawSvg, data);
   }
 
   getName() {
     return this.name;
   }
 
-  setChartParameters(rawSvg, twitterData) {
-
+  setChartParameters(rawSvg) {
     this.xScale = D3.scale.linear()
       .domain([this.twitterData[0].day, this.twitterData[this.twitterData.length - 1].day])
       .range([this.padding + 5, rawSvg.clientWidth - this.padding]);
 
-    this.yScale = D3.scale.linear()
-      .domain([d3.min(twitterData, (d) => d.total), d3.max(twitterData, (d) => d.total)])
+    this.twitterYScale = D3.scale.linear()
+      .domain([d3.min(this.twitterData, (d) => d.total), d3.max(this.twitterData, (d) => d.total)])
+      .range([rawSvg.clientHeight - this.padding, this.padding]);
+
+    this.facebookYScale = D3.scale.linear()
+      .domain([d3.min(this.facebookData, (d) => d.total), d3.max(this.facebookData, (d) => d.total)])
+      .range([rawSvg.clientHeight - this.padding, this.padding]);
+
+    this.deltaYScale = D3.scale.linear()
+      .domain([d3.min(this.deltaData, (d) => d.total), d3.max(this.deltaData, (d) => d.total)])
       .range([rawSvg.clientHeight - this.padding, this.padding]);
 
     this.xAxisGen = D3.svg.axis()
@@ -42,33 +53,40 @@ class TargetService {
       .orient("bottom")
       .ticks(12)
 
-    this.yAxisGen = D3.svg.axis()
-      .scale(this.yScale)
+    this.twitterYAxisGen = D3.svg.axis()
+      .scale(this.twitterYScale)
+      .orient("right")
+      .ticks(5)
+      .tickFormat(d3.format("0s"));
+
+    this.facebookYAxisGen = D3.svg.axis()
+      .scale(this.facebookYScale)
       .orient("left")
       .ticks(5)
       .tickFormat(d3.format("0s"));
 
-    this.lineFun = D3.svg.line()
-      .x((d) => {
-        // console.log(d.day)
-        return this.xScale(d.day)
-      })
-      .y((d) => {
-        // console.log(d.total);
-        return this.yScale(d.total)
-      })
+    this.deltaYAxisGen = D3.svg.axis()
+      .scale(this.deltaYScale)
+      .orient("left")
+      .ticks(5)
+      .tickFormat(d3.format("0s"));
+
+    this.twitterArea = D3.svg.area()
+      .x((d) => this.xScale(d.day))
+      .y((d) => this.twitterYScale(d.total))
+      .y0(rawSvg.clientHeight - (this.padding + 5))
       .interpolate("basis");
 
-    this.area = D3.svg.area()
-      .x((d) => {
-        // console.log(d.day)
-        return this.xScale(d.day)
-      })
-      .y((d) => {
-        // console.log(d.total);
-        return this.yScale(d.total)
-      })
+    this.facebookArea = D3.svg.area()
+      .x((d) => this.xScale(d.day))
+      .y((d) => this.facebookYScale(d.total))
       .y0(rawSvg.clientHeight - (this.padding + 5))
+      .interpolate("basis");
+
+
+    this.lineFun = D3.svg.line()
+      .x((d) => this.xScale(d.day))
+      .y((d) => this.deltaYScale(d.total))
       .interpolate("basis");
   }
 
@@ -85,16 +103,17 @@ class TargetService {
     return preppedData;
   }
 
-  drawLineChart(rawSvg, data) {
-
+  drawCharts(rawSvg, data) {
     if (data.metrics === undefined) return;
 
     let twitterData = data.metrics.social[0]['twitterMetric'];
     let facebookData = data.metrics.social[1]['facebookMetric'];
+    let deltaData = data.metrics.delta;
     this.twitterData = this.prepData(twitterData);
     this.facebookData = this.prepData(facebookData);
+    this.deltaData = this.prepData(deltaData);
 
-    this.setChartParameters(rawSvg, this.twitterData);
+    this.setChartParameters(rawSvg);
 
     if (!this.initialized) {
       this.instantiateGraph(rawSvg);
@@ -104,44 +123,77 @@ class TargetService {
   }
 
   instantiateGraph(rawSvg) {
-    let axisStyles = { 
-      'stroke': '#086788',
-      'fill': 'none',
-      'stroke-width': '0.5px',
-      'font-size': '0.5em'
-    };
 
+    // Axes.
     this.svg.append('svg:g')
       .classed("svg-container", true)
-      .attr('class', 'x axis')
-      .style(axisStyles)
+      .attr('class', 'x-axis')
       .attr('transform', `translate(0, ${rawSvg.clientHeight - this.padding})`)
       .call(this.xAxisGen);
 
     this.svg.append('svg:g')
       .classed("svg-container", true)
-      .attr('class', 'y axis')
-      .style(axisStyles)
-      .attr('transform', `translate(${this.padding}, 0)`)
-      .call(this.yAxisGen);
+      .attr('class', 'y-axis-twitter')
+      .attr('transform', `translate(${rawSvg.clientWidth - this.padding + 5}, -5)`)
+      .call(this.twitterYAxisGen);
 
+    this.svg.append('svg:g')
+      .classed("svg-container", true)
+      .attr('class', 'y-axis-facebook')
+      .attr('transform', `translate(${rawSvg.clientWidth - this.padding + 5}, -5)`)
+      .call(this.facebookYAxisGen);
+
+    this.svg.append('svg:g')
+      .classed("svg-container", true)
+      .attr('class', 'y-axis-delta')
+      .attr('transform', `translate(${this.padding + 5}, -5)`)
+      .call(this.deltaYAxisGen);
+
+    // Areas.
     this.svg.append('svg:path')
-      .attr('d', this.area(this.twitterData))
+      .attr('d', this.twitterArea(this.twitterData))
       .attr('stroke', '#07A0C3')
       .attr('stroke-width', 2)
-      .attr('class', 'area')
+      .attr('class', 'twitter-area');
+
+    this.svg.append('svg:path')
+      .attr('d', this.facebookArea(this.facebookData))
+      .attr('stroke', '#07A0C3')
+      .attr('stroke-width', 2)
+      .attr('class', 'facebook-area');
+
+    // Lines.
+    this.svg.append('svg:path')
+      .attr('d', this.lineFun(this.deltaData))
+      .attr('stroke', '#07A0C3')
+      .attr('stroke-width', 2)
+      .attr('class', 'delta-line');
 
     this.initialized = true;
   }
 
   updateGraph() {
-    this.svg.select(".area")
+    this.svg.select(".twitter-area")
       .transition()
       .duration(1000)
       .ease("cubic")
-      .attr("d", this.area(this.twitterData))
+      .attr("d", this.twitterArea(this.twitterData))
 
-    this.svg.selectAll("g.y.axis").call(this.yAxisGen);
+    this.svg.select(".facebook-area")
+      .transition()
+      .duration(1000)
+      .ease("cubic")
+      .attr("d", this.facebookArea(this.facebookData))
+
+    this.svg.select(".delta-line")
+      .transition()
+      .duration(1000)
+      .ease("cubic")
+      .attr("d", this.lineFun(this.deltaData))
+
+    this.svg.selectAll("g.y-axis-twitter").call(this.twitterYAxisGen);
+    this.svg.selectAll("g.y-axis-facebook").call(this.facebookYAxisGen);
+    this.svg.selectAll("g.y-axis-delta").call(this.deltaYAxisGen);
     this.svg.selectAll("g.x.axis").call(this.xAxisGen);
   }
 
